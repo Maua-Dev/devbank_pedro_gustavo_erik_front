6267db34a771c0c28967e000b839f4a98e5bb417
PedroOSanches <pedrohenriqueoliveirasanches3@gmail.com>
2026-04-25 16:30:23 -0300

    feat: Adiciona Tailwind e atualiza componente BotaoNavegacao para receber codigo Tailwindcss

--- src/components/CardNotas.tsx
+++ src/components/CardNotas.tsx
@@ -7,7 +7,7 @@ interface CardNotas{
 export default function CardNotas({valorNota}: CardNotas){
     const [qntdNotas, setQntdNotas] = useState(0)
     return(
-        <div style={{display: "flex", flexDirection: "column", alignItems: "center"}}>
+        <div className="">
             <p>{valorNota}R$</p>
             <div style={{display: "flex", gap: "5px"}}>
                 <button onClick={() => {qntdNotas > 0 ? setQntdNotas(qntdNotas - 1) : alert("Não à notas neste valor depositadas")}}>-</button>
