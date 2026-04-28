532434dc6c15aceadc3c0650ec7407cf5420d28a
PedroOSanches <pedrohenriqueoliveirasanches3@gmail.com>
2026-04-28 00:23:58 -0300

    Feat: Estiliza CardNotas e inicia estilizacao de TelaDeposito. Traz useState para TelaDeposito

--- src/components/CardNotas.tsx
+++ src/components/CardNotas.tsx
@@ -1,19 +1,27 @@
 import { useState } from "react";
-
 interface CardNotas{
     valorNota: number;
-    className?: string;
+    onchange: (valor: number, qntde: number) => void
 }
 
-export default function CardNotas({valorNota, className}: CardNotas){
+export default function CardNotas({valorNota, onchange}: CardNotas){
+
     const [qntdNotas, setQntdNotas] = useState(0)
+
+    function alterarQntde(novaQnd: number){
+        setQntdNotas(novaQnd);
+        onchange(valorNota, novaQnd);
+    }
+
     return(
         <div className="">
-            <p>{valorNota}R$</p>
-            <div style={{display: "flex", gap: "5px"}}>
-                <button className={className} onClick={() => {qntdNotas > 0 ? setQntdNotas(qntdNotas - 1) : alert("Não à notas neste valor depositadas")}}>-</button>
-                <p>{qntdNotas}</p>
-                <button className={className} onClick={() => {setQntdNotas(qntdNotas + 1)}}>+</button>
+            <div className="flex flex-col gap-[13.5px]">
+                <p className="bg-[#567DB7] w-73.75 h-[115.5px] text-center rounded-[30px]">{valorNota}R$</p>
+                <div className="flex flex-row justify-around">
+                    <button className="w-16.75 h-10 bg-[#567DB7] rounded-[30px]" onClick={() => {qntdNotas > 0 ? alterarQntde(qntdNotas - 1) : alert("Não à notas neste valor selecionadas")}}>-</button>
+                    <p>{qntdNotas}</p>
+                    <button className="w-16.75 h-10 rounded-[30px] bg-[#567DB7] " onClick={() => {alterarQntde(qntdNotas + 1)}}>+</button>
+                </div>
             </div>
         </div>
     )
--- src/pages/TelaDeposito.tsx
+++ src/pages/TelaDeposito.tsx
@@ -1,28 +1,53 @@
-import CardNotas from "../components/CardNotas"
-import BotaoNavegacao from "../components/BotaoNavegacao"
+  import CardNotas from "../components/CardNotas"
+  import BotaoNavegacao from "../components/BotaoNavegacao"
+  import { useState } from "react";
 
-const notas = [
-  {valorNota: 2},
-  {valorNota: 5},
-  {valorNota: 10},
-  {valorNota: 20},
-  {valorNota: 50},
-  {valorNota: 100},
-  {valorNota: 200},
-]
+  const notas = [
+    {valorNota: 2},
+    {valorNota: 5},
+    {valorNota: 10},
+    {valorNota: 20},
+    {valorNota: 50},
+    {valorNota: 100},
+    {valorNota: 200},
+  ];
 
-function TelaDeposito() {
-  return <div>
-    <h1>Tela Depósito</h1>
-    <p>Selecione as Celulas que Você deseja</p>
-    <div style={{display: "flex", gap: "10px"}}>
-      {notas.map((nota) => (<CardNotas valorNota={nota.valorNota} />))}
+  export default function TelaDeposito() {
+    const saldo = 1000
+    const [notasSelecionadas, setNotasSelecionadas] = useState<{[chave: number]: number}>({});
+
+    function atualizarNotas(valor: number, qntde: number){
+      setNotasSelecionadas((prev) => {
+        const novo = {...prev};
+        
+        if (qntde === 0){
+          delete novo[valor];
+        } else {
+          novo[valor] = qntde;
+        }
+
+        return novo;
+        
+      });
+    }
+
+    const totalDepositado = Object.entries(notasSelecionadas).reduce((totalDepositado, [valor, qnde]) => totalDepositado + Number.parseInt(valor) * qnde, 0)
+    return <div>
+      <header className="flex ">
+        <h1>Saldo atual: {0.00}</h1>
+        <h2>Selecione as cédulas e a quantidade que você deseja</h2>
+      </header>
+      <div>
+        <p>Quantidade Depositada R$ {totalDepositado}</p>
+        <p>Quantidade Total R$ {saldo + totalDepositado}</p>
+      </div>
+      <div className="flex gap-2">
+        <BotaoNavegacao className="bg-[#567DB7] w-57.5 h-27.25 rounded-[30px]" nome="Voltar" rota="conta" />
+        <button className="bg-[#567DB7] w-57.5 h-27.25 rounded-[30px]" >Depositar</button>
+      </div>
+      <p>Selecione as Celulas que Você deseja</p>
+      <div className="grid grid-cols-2 gap-9">
+        {notas.map((nota) => (<CardNotas key={nota.valorNota} valorNota={nota.valorNota} onchange={atualizarNotas}/>))}
+      </div>
     </div>
-    <div style={{display: "flex", gap:"10px"}}>
-    <BotaoNavegacao nome="Voltar" rota="conta" />
-    <button>Depositar</button>
-    </div>
-  </div>
-}
-
-export default TelaDeposito
+  } 
